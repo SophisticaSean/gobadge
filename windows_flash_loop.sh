67a0ba08927d991f837dc0428770bf2c9b0b7afd
Sean Lewis <sensaisean@gmail.com>
2022-10-11 22:16:28 -0600

    test

--- windows_flash_loop.sh
+++ windows_flash_loop.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 # this will sit and run git pull, if it finds changes it'll run make flash
 
+
 while true; do
   git pull --progress | grep "Already up to date" || make flash
   sleep 1
