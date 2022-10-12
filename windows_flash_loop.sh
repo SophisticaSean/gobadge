ed8aab785d278cb4c8b24e817871de12f4fc255f
Sean Lewis <sensaisean@gmail.com>
2022-10-11 22:10:38 -0600

    test

--- windows_flash_loop.sh
+++ windows_flash_loop.sh
@@ -1,7 +1,6 @@
 # !/bin/bash
 # this will sit and run git pull, if it finds changes it'll run make flash
 
-
 while true; do
   git pull | grep "remote: " && echo "flashing.." && make flash
   sleep 1
